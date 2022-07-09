99a820be4cd36d407d92c74f4da52e0533c74e46
kartik31002 <95395681+kartik31002@users.noreply.github.com>
2022-07-09 15:45:03 +0530

    Update models.py

--- main/models.py
+++ main/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 class Movie(models.Model): # we are creating a table named Movie
     #fields for the Movie table
-    name = models.CharField(max_length=350)
-    director= models.CharField(max_length=350)
-    cast= models.CharField(max_length=1000)
+    name = models.CharField(max_length=350, unique=True)
+    director= models.CharField(max_length=350, null=True)
+    cast= models.CharField(max_length=1000, null=True)
     description= models.TextField(max_length=5000)
     release_date= models.DateField()
     averageRating = models.FloatField(default=0)
@@ -20,3 +20,18 @@ class Movie(models.Model): # we are creating a table named Movie
 
         #now after creating a table we need to register it to admin.py
         #and after making any changes to database we need to run migrations
+
+class Review(models.Model): # we are creating a table named Movie
+    #fields for the Movie table
+    name = models.CharField(max_length=350)
+    rating = models.FloatField(default=0)
+    review = models.TextField()
+    date = models.DateField()
+    user = models.CharField(max_length=100)
+
+
+    def __str__(self): #function called string returns whatever you want in the string version in django-admin
+        return self.name
+
+    def __unicode__(self):
+        return self.name
